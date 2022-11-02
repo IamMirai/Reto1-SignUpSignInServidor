887f9dd4a1d5bd0b7bc7ac5dc5f24b086bddb968
MikelBermejo <mikelbermejofdez@gmail.com>
2022-11-02 13:04:27 +0100

    Pool connection done

---
+++ src/pool/ServerConnection.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pool;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author User
+ */
+public class ServerConnection {
+    private Connection connection;
+    private boolean active;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+    
+}
